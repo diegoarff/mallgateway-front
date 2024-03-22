7ea63c2aafa635f3c0b23af071f958a8a6205808
diegoarff <diegorinconf1@gmail.com>
2024-03-21 21:58:32 -0400

    chore: remove unused searchvisible store state

--- src/stores/global/search.js
+++ src/stores/global/search.js
@@ -1,7 +1,4 @@
 export const createSearchSlice = (set) => ({
   searchQuery: "",
-  isSearchVisible: false,
-
   setSearchQuery: (query) => set({ searchQuery: query }),
-  setIsSearchVisible: (visible) => set({ isSearchVisible: visible }),
 });
