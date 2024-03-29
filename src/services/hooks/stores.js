5180c79d270171d340bb18fd21e8e5a0d3f03dfa
diegoarff <diegorinconf1@gmail.com>
2024-03-29 14:00:12 -0400

    refactor: services file structure

--- src/services/hooks/stores.js
+++ src/services/hooks/stores.js
@@ -1,9 +1,54 @@
-import { useQuery } from "@tanstack/react-query";
-import { getStores } from "../api/stores";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import {
+  createStore,
+  getStoreCategories,
+  getStores,
+  processStoreCategories,
+} from "../api/stores";
+import { useGlobalStore } from "../../stores/global";
 
 export const useGetStores = () => {
   return useQuery({
     queryKey: ["stores"],
     queryFn: getStores,
+  });
+};
+
+export const useCreateStore = () => {
+  const queryClient = useQueryClient();
+  const showSnackbar = useGlobalStore((state) => state.showSnackbar);
+
+  return useMutation({
+    mutationFn: (store) => createStore(store),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["stores"] });
+      showSnackbar("Tienda creada");
+    },
+    onError: (error) => {
+      showSnackbar(error);
+    },
+  });
+};
+
+export const useGetStoreCategories = () => {
+  return useQuery({
+    queryKey: ["store-categories"],
+    queryFn: getStoreCategories,
+  });
+};
+
+export const useProcessStoreCategories = () => {
+  const queryClient = useQueryClient();
+  const showSnackbar = useGlobalStore((state) => state.showSnackbar);
+
+  return useMutation({
+    mutationFn: (categories) => processStoreCategories(categories),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["store-categories"] });
+      showSnackbar("Categorías actualizadas");
+    },
+    onError: (error) => {
+      showSnackbar(error);
+    },
   });
 };
